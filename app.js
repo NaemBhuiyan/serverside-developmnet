3b119aac1506aa259441c2b03b44a3828b2f1d0d
Naem Bhuiyan <hopingboynaem@gmail.com>
2020-05-06 17:03:42 +0600

    authentication with passport

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+var passport = require("passport");
 
+var authenticate = require("./authenticate");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const promoRouter = require("./routes/promoRouter");
@@ -44,25 +46,21 @@ app.use(
     store: new FileStore(),
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
     err.status = 403;
-    return next(err);
+    next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
